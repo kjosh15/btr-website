33b8ed213a7ebd7abd7b19adf9b0b2cf6e2d3f9b
Josh Klein <josh@josh.is>
2025-10-27 10:48:36 +0000

    Fix asset paths and language toggle

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
-const isDev = process.env.NODE_ENV === 'development';
-
 export default defineConfig({
-  base: isDev ? '/' : './',
+  base: '/',
   trailingSlash: 'never',
   integrations: [
     tailwind({
@@ -12,7 +10,4 @@ export default defineConfig({
     }),
   ],
   site: 'https://btr.is',
-  build: {
-    assetsPrefix: isDev ? undefined : '.',
-  },
 });
